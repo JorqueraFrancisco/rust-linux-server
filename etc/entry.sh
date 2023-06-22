06ed9bb0f34e57f11825e90e75a09161adff78ac
JorqueraFrancisco <33900932+JorqueraFrancisco@users.noreply.github.com>
2023-06-22 17:18:46 -0400

    added custom map

--- etc/entry.sh
+++ etc/entry.sh
@@ -3,7 +3,7 @@
 ./RustDedicated -batchmode \
 +server.port ${SERVER_PORT} \
 +server.queryport ${SERVER_QUERYPORT} \
-+server.level "Procedural Map" \
++server.levelurl "https://www.dropbox.com/s/zblgbxb4xaa6ny4/edited.map?dl=1" \
 +server.hostname  ${SERVER_HOSTNAME} \
 +server.identity ${SERVER_IDENTITY} \
 +server.seed ${SERVER_SEED} \
